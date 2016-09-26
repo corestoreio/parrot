48ea4356a63789223ba9da8b5994d79a8e925c88
Anthony N. Simon <anthonynsimon@gmail.com>
2016-09-26 17:56:18 +0200

    Add doc key func

--- model/models.go
+++ model/models.go
@@ -1,10 +1,19 @@
 package model
 
+import "fmt"
+
 type DocStorer interface {
 	GetDoc(id int) (*Document, error)
 	CreateDoc(doc *Document) error
 	UpdateDoc(doc *Document) error
 	DeleteDoc(id int) (int, error)
+}
+
+type ProjectStorer interface {
+	GetProject(id int) (*Project, error)
+	CreateProject(doc *Project) error
+	// UpdateProject(doc *Project) error
+	// DeleteProject(id int) (int, error)
 }
 
 func NewDocument(id int) *Document {
@@ -35,4 +44,12 @@ type Project struct {
 	ID    int            `db:"id" json:"id"`
 	Keys  []string       `db:"keys" json:"keys"`
 	Langs map[string]int `db:"langs" json:"langs"`
+}
+
+func (p *Project) DocKey(lang string) (int, error) {
+	v, ok := p.Langs[lang]
+	if !ok {
+		return 0, fmt.Errorf("no doc for lang key '%s'", lang)
+	}
+	return v, nil
 }
