c2c74f9e16b66fea94db0b9e191f1d8edeaeb533
Anthony N. Simon <anthonynsimon@gmail.com>
2016-09-27 11:42:24 +0200

    Refactor api helpers

--- api/helpers.go
+++ api/helpers.go
@@ -2,23 +2,32 @@ package api
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
+)
+
+var (
+	jsonContentType = "application/json; charset=utf-8"
 )
 
 type apiHandler func(http.ResponseWriter, *http.Request) (int, error)
 
 func (h apiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", jsonContentType)
 	status, err := h(w, r)
 	if err != nil {
+		// TODO log err
 		w.WriteHeader(status)
-		w.Header().Set("content-type", "application/json; charset=utf-8")
-		data, _ := json.Marshal(map[string]interface{}{
+		data, err := json.Marshal(map[string]interface{}{
 			"status": status,
 			"error":  http.StatusText(status),
 		})
+		if err != nil {
+			// log
+		}
 		_, err = w.Write(data)
-		log.Println(err)
+		if err != nil {
+			// log
+		}
 	}
 }
 
@@ -28,7 +37,6 @@ func writeJSON(w http.ResponseWriter, s int, v interface{}) (int, error) {
 		return http.StatusInternalServerError, err
 	}
 
-	w.Header().Set("content-type", "application/json; charset=utf-8")
 	_, err = w.Write(data)
 	if err != nil {
 		return http.StatusInternalServerError, err
