82f3fd6d23fec2bb83dedef22fc06257d01c2e3d
Anthony N. Simon <anthonynsimon@gmail.com>
2016-09-27 12:50:51 +0200

    Rename ParrotError

--- errors/errors.go
+++ errors/errors.go
@@ -6,15 +6,15 @@ var (
 	ErrNotFound = New(404, "not found")
 )
 
-type ParrotError struct {
+type Error struct {
 	Code    int
 	Message string
 }
 
-func (e *ParrotError) Error() string {
+func (e *Error) Error() string {
 	return fmt.Sprintf("error: %d %s", e.Code, e.Message)
 }
 
-func New(c int, m string) *ParrotError {
-	return &ParrotError{Code: c, Message: m}
+func New(c int, m string) *Error {
+	return &Error{Code: c, Message: m}
 }
