b1027a0cb118375e43d0f048709c7f903babbc1b
Anthony N. Simon <anthonynsimon@gmail.com>
2016-09-29 18:23:13 +0200

    Add user store interface

--- model/user.go
+++ model/user.go
@@ -1,5 +1,12 @@
 package model
 
+type UserStorer interface {
+	GetUser(int) (*User, error)
+	CreateUser(*User) error
+	UpdateUser(*User) error
+	DeleteUser(int) (int, error)
+}
+
 type User struct {
 	ID       int    `json:"id"`
 	Email    string `json:"email`
