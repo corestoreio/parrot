06e915f730eb154099235f5a32c0b77ff4dbdf39
Anthony N. Simon <anthonynsimon@gmail.com>
2016-10-27 10:30:09 +0200

    Add hot reload server

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,21 +1,31 @@
-var path = require('path')
+var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
     output: {
-        path: path.resolve(__dirname, './../static/dist'),
-        filename: "bundle.js"
+        path: path.resolve(__dirname, '/static/dist'),
+        filename: 'bundle.js',
+        publicPath: path.resolve(__dirname, '/static/')
     },
     module: {
+        plugins: [
+            new webpack.HotModuleReplacementPlugin()
+        ],
         loaders: [
-        {
-            test: /.jsx?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            query: {
-            presets: ['es2015', 'react']
+            {
+                test: /\.js$/,
+                loaders: [
+                    'react-hot',
+                    'babel-loader'
+                ],
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src')
             }
-        }
         ]
     }
 }
