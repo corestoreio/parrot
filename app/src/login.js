a2612bf5403ec2bd6ba0db343231b42d97cf32da
Anthony N. Simon <anthonynsimon@gmail.com>
2016-10-27 11:36:39 +0200

    Add basic forms

--- app/src/login.js
+++ app/src/login.js
@@ -1,108 +1,40 @@
 import React from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 class Register extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
-
-    setValue(field, event) {
-        var object = {};
-        object[field] = event.target.value;
-        this.setState(object);
-    }
-
-    register(event) {
-        event.preventDefault();
-    }
-
     render() {
         return (
             <div>
-                <h1>Register</h1>
-                <form onSubmit={this.register.bind(this)}>
-                    <div className="form-group">
-                        <label htmlFor="emailInput">Email address</label>
-                        <input
-                            type="email"
-                            onChange={this.setValue.bind(this, 'email')}
-                            className="form-control"
-                            id="emailInput"
-                            placeholder="Enter email"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="passwordInput">Password</label>
-                        <input
-                            type="password"
-                            onChange={this.setValue.bind(this, 'password')}
-                            className="form-control"
-                            id="passwordInput"
-                            placeholder="Password"/>
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary">
-                    Register
-                    </button>
-                </form>
+                <TextField
+                    hintText="Your email"
+                    floatingLabelText="Email"
+                /><br />
+                <TextField
+                    hintText="Your password"
+                    floatingLabelText="Password"
+                    type="password"
+                /><br />
+                <RaisedButton label="Register" primary={true} style={{margin:12}} />
             </div>
         )
     }
 }
 
 class Login extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
-
-    setValue(field, event) {
-        var object = {};
-        object[field] = event.target.value;
-        this.setState(object);
-    }
-
-    login(event) {
-        event.preventDefault();
-    }
-
     render() {
         return (
             <div>
-                <h1>Login</h1>
-                <form onSubmit={this.login.bind(this)}>
-                    <div className="form-group">
-                        <label htmlFor="emailInput">Email address</label>
-                        <input
-                            type="email"
-                            onChange={this.setValue.bind(this, 'email')}
-                            className="form-control"
-                            id="emailInput"
-                            placeholder="Enter email"/>
-                        <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                    </div>
-                    <div className="form-group">
-                        <label htmlFor="passwordInput">Password</label>
-                        <input
-                            type="password"
-                            onChange={this.setValue.bind(this, 'password')}
-                            className="form-control"
-                            id="passwordInput"
-                            placeholder="Password"/>
-                    </div>
-                    <button
-                        type="submit"
-                        className="btn btn-primary">
-                    Login
-                    </button>
-                </form>
+                <TextField
+                    hintText="Your email"
+                    floatingLabelText="Email"
+                /><br />
+                <TextField
+                    hintText="Your password"
+                    floatingLabelText="Password"
+                    type="password"
+                /><br />
+                <RaisedButton label="Login" primary={true} style={{margin:12}} />
             </div>
         )
     }
