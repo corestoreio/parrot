93c76d383aa84719a60f8dc668be3f5f6be1b126
Anthony N. Simon <anthonynsimon@gmail.com>
2016-10-27 12:21:01 +0200

    Remove unused import

--- app/src/router.js
+++ app/src/router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router'
+import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import App from './app';
 import Home from './components/home';
 import NotFound from './components/404';
