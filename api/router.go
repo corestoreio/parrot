a60dd778143ba14309819d4a1c5538350632d3dd
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-11-09 22:37:13 +0100

    Refactor router and route paths

--- api/router.go
+++ api/router.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/anthonynsimon/parrot/auth"
 	"github.com/anthonynsimon/parrot/datastore"
-	"github.com/anthonynsimon/parrot/paths"
 	"github.com/anthonynsimon/parrot/render"
 	"github.com/pressly/chi"
 )
@@ -20,38 +19,38 @@ func NewRouter(ds datastore.Store, authProvider auth.Provider) http.Handler {
 	router := chi.NewRouter()
 	router.Use(cors)
 
-	router.Get(paths.PingPath, ping)
-	router.Post(paths.AuthenticatePath, authenticate(authProvider))
+	router.Get("/ping", ping)
+	router.Post("/authenticate", authenticate(authProvider))
 
-	router.Route(paths.UsersPath, func(dr chi.Router) {
+	router.Route("/users", func(dr chi.Router) {
 		dr.Post("/", createUser)
-		dr.Route("/self", func(pr chi.Router) {
-			pr.Get("/projects", getUserProjects)
-		})
 	})
 
-	router.Route(paths.ProjectsPath, func(pr chi.Router) {
+	router.Route("/projects", func(pr chi.Router) {
 		// Past this point, all routes require a valid token
 		pr.Use(handleToken)
 		pr.Get("/", getUserProjects)
 		pr.Post("/", createProject)
-		pr.Get("/:projectID", mustAuthorize(canViewProject, showProject))
-		pr.Put("/:projectID", mustAuthorize(canUpdateProject, updateProject))
-		pr.Delete("/:projectID", mustAuthorize(canDeleteProject, deleteProject))
 
-		pr.Route("/:projectID"+paths.UsersPath, func(dr chi.Router) {
-			dr.Get("/", mustAuthorize(canViewProjectRoles, getProjectUsers))
-			dr.Post("/", mustAuthorize(canAssignRoles, assignProjectUser))
-			dr.Put("/:userID", mustAuthorize(canUpdateRoles, updateProjectUser))
-			dr.Delete("/:userID", mustAuthorize(canRevokeRoles, revokeProjectUser))
-		})
+		pr.Route("/:projectID", func(dr chi.Router) {
+			pr.Get("/", mustAuthorize(canViewProject, showProject))
+			pr.Put("/", mustAuthorize(canUpdateProject, updateProject))
+			pr.Delete("/", mustAuthorize(canDeleteProject, deleteProject))
 
-		pr.Route("/:projectID"+paths.LocalesPath, func(dr chi.Router) {
-			dr.Post("/", mustAuthorize(canCreateLocales, createLocale))
-			dr.Get("/", mustAuthorize(canViewLocales, findLocales))
-			dr.Get("/:localeID", mustAuthorize(canViewLocales, showLocale))
-			dr.Put("/:localeID", mustAuthorize(canUpdateLocales, updateLocale))
-			dr.Delete("/:localeID", mustAuthorize(canDeleteLocales, deleteLocale))
+			pr.Route("/users", func(dr chi.Router) {
+				dr.Get("/", mustAuthorize(canViewProjectRoles, getProjectUsers))
+				dr.Post("/", mustAuthorize(canAssignRoles, assignProjectUser))
+				dr.Put("/:userID", mustAuthorize(canUpdateRoles, updateProjectUser))
+				dr.Delete("/:userID", mustAuthorize(canRevokeRoles, revokeProjectUser))
+			})
+
+			pr.Route("/locales", func(dr chi.Router) {
+				dr.Get("/", mustAuthorize(canViewLocales, findLocales))
+				dr.Post("/", mustAuthorize(canCreateLocales, createLocale))
+				dr.Get("/:localeID", mustAuthorize(canViewLocales, showLocale))
+				dr.Put("/:localeID", mustAuthorize(canUpdateLocales, updateLocale))
+				dr.Delete("/:localeID", mustAuthorize(canDeleteLocales, deleteLocale))
+			})
 		})
 	})
 
