00b574573cec75c7b71c1f4a9ac2d9dd4e5a0ec0
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-11-15 19:57:15 +0100

    Fix command chain

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Starting Parrot containers..."
-docker-compose up --build -d
-docker exec parrot_api_1 pgmgr db create && pgmgr db migrate
+docker-compose up -d
+docker exec parrot_api_1 pgmgr db create
+docker exec parrot_api_1 pgmgr db migrate
 docker-compose logs -f
