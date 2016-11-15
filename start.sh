03ce4c7ce8869100fcb1e9bb05ac4fae9fa0b76a
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-11-15 19:41:32 +0100

    Add pgmgr create db before migration

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 echo "Starting Parrot containers..."
 docker-compose up --build -d
-docker exec parrot_api_1 pgmgr db migrate
+docker exec parrot_api_1 pgmgr db create && pgmgr db migrate
 docker-compose logs -f
