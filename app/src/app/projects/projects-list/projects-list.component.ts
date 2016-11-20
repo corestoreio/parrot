46716f660cbc94d3d7cc0b030b11e6dfcc9be658
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-11-21 00:05:02 +0100

    Add styles to project-list

--- app/src/app/projects/projects-list/projects-list.component.ts
+++ app/src/app/projects/projects-list/projects-list.component.ts
@@ -8,15 +8,17 @@ import { ProjectsService } from './../services/projects.service';
 })
 export class ProjectsListComponent implements OnInit {
   private projects;
+  private loading = false;
 
   constructor(private projectsService: ProjectsService) {
     this.projects = [];
   }
 
   ngOnInit() {
+    this.loading = true;
     this.projectsService.getProjects().subscribe(
-      res => { this.projects = res; },
-      err => { console.log(err); }
+      res => { this.projects = res; this.loading = false; },
+      err => { console.log(err); this.loading = false; }
     )
   }
 }
