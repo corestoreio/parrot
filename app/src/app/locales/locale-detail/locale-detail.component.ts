131ada99ba7b451ac9805ae3bd3c879d87c1e881
Anthony Najjar <anthonynsimon@gmail.com>
2016-11-23 14:30:46 +0100

    Add data grid loading

--- app/src/app/locales/locale-detail/locale-detail.component.ts
+++ app/src/app/locales/locale-detail/locale-detail.component.ts
@@ -9,15 +9,18 @@ import { LocalesService } from './../services/locales.service';
 })
 export class LocaleDetailComponent {
     private locale;
+    private loading = false;
 
     constructor(private localesService: LocalesService, private route: ActivatedRoute) { }
 
     ngOnInit() {
         let projectId = +this.route.snapshot.params['projectId'];
         let localeIdent = this.route.snapshot.params['localeIdent'];
+        this.loading = true;
         this.localesService.fetchLocale(projectId, localeIdent).subscribe(
             res => { this.locale = res; },
-            err => { console.log(err); }
+            err => { console.log(err); },
+            () => { this.loading = false; }
         );
     }
 }
