4b5bb3a3818ab7295fa1fa1f9020bac34f137658
Anthony Najjar <anthonynsimon@gmail.com>
2016-11-24 11:16:32 +0100

    Add page level service injector

--- app/src/app/pages/project/project-page.component.ts
+++ app/src/app/pages/project/project-page.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { LocalesService } from './../../locales/services/locales.service';
+
 @Component({
+    providers: [LocalesService],
     selector: 'project-page',
     templateUrl: 'project-page.component.html'
 })
