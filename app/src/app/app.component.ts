ecfbf01f708cbd62665647e90e6731ec0034ea1a
Anthony Najjar <anthonynsimon@gmail.com>
2016-11-24 17:50:36 +0100

    Add projects list side nav

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,16 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthService } from './auth';
+import { ProjectsService } from './projects';
 
 @Component({
   selector: 'root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Parrot';
+  private projects;
+  private loading = false;
 
-  constructor(private router: Router, private auth: AuthService) { }
+  constructor(private router: Router, private auth: AuthService, private projectsService: ProjectsService) { }
+
+  ngOnInit() {
+    this.projectsService.projects.subscribe(
+      projects => { this.projects = projects; },
+      err => { console.log(err); }
+    );
+    this.fetchProjects();
+  }
+
+  fetchProjects() {
+    this.loading = true;
+    this.projectsService.getProjects().subscribe(
+      () => { },
+      err => { console.log(err); },
+      () => { this.loading = false; }
+    );
+  }
 
   logout() {
     this.auth.logout();
