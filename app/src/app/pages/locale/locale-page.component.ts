ceb17f09017b2c3712f056fe7e2463b722e1cd02
Anthony Najjar <anthonynsimon@gmail.com>
2016-11-24 12:10:24 +0100

    Set page level locales service

--- app/src/app/pages/locale/locale-page.component.ts
+++ app/src/app/pages/locale/locale-page.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LocalesService } from './../../locales/services/locales.service';
+
 @Component({
+    providers: [LocalesService],
     selector: 'locale-page',
     templateUrl: 'locale-page.component.html'
 })
