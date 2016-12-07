b89e5e6c52d8f8c13271c030e32e83089eb7d8fd
Anthony Najjar <anthonynsimon@gmail.com>
2016-12-07 12:50:50 +0100

    Add basic restore service

--- app/src/app/api-access/app-detail/app-detail.component.ts
+++ app/src/app/api-access/app-detail/app-detail.component.ts
@@ -1,17 +1,36 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Application } from './../model/app';
+import { RestoreItemService } from './../../shared/restore-item.service';
 
 @Component({
+    providers: [RestoreItemService],
     selector: 'app-detail',
     templateUrl: 'app-detail.component.html',
     styleUrls: ['app-detail.component.css']
 })
 export class AppDetailComponent implements OnInit {
     @Input()
-    private app: Application;
+    set app(value: Application) {
+        this.restoreService.setOriginal(value);
+    }
 
-    constructor() { }
+    set _app(value: Application) {
+        this.restoreService.setCurrent(value);
+    }
+
+    get _app(): Application {
+        return this.restoreService.getCurrent();
+    }
+
+    constructor(private restoreService: RestoreItemService<Application>) { }
 
     ngOnInit() { }
+
+    cancelChanges() {
+        this.restoreService.restoreOriginal();
+    }
+
+    saveChanges() {
+    }
 }
