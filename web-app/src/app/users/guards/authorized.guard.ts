d67fc090e00ad8c9884ba7ccb26a5b1e7c8eb266
Anthony Najjar <anthonynsimon@gmail.com>
2016-12-15 15:52:37 +0100

    Remove unsued import

--- web-app/src/app/users/guards/authorized.guard.ts
+++ web-app/src/app/users/guards/authorized.guard.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
-import { Subject } from 'rxjs/Subject';
 
 import { UserService } from './../services/user.service';
 
