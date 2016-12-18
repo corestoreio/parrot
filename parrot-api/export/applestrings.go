2845e8ca44d59bc64574e281104d74402b2d7877
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-12-18 14:01:23 +0100

    Fix apple strings format

--- parrot-api/export/applestrings.go
+++ parrot-api/export/applestrings.go
@@ -18,7 +18,7 @@ func (e *AppleStrings) Export(locale *model.Locale) ([]byte, error) {
 	buf := bytes.NewBuffer(nil)
 
 	for k, v := range locale.Pairs {
-		_, err := buf.WriteString(fmt.Sprintf("\"%s\" = \"%s\"\n\n", k, v))
+		_, err := buf.WriteString(fmt.Sprintf("\"%s\" = \"%s\";\n", k, v))
 		if err != nil {
 			return nil, err
 		}
