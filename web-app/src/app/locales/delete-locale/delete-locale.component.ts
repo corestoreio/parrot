debe86f76bdf55f0071b46e617d8b93ecf3d39b0
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-12-21 00:12:11 +0100

    Fix locale is null

--- web-app/src/app/locales/delete-locale/delete-locale.component.ts
+++ web-app/src/app/locales/delete-locale/delete-locale.component.ts
@@ -58,6 +58,9 @@ export class DeleteLocaleComponent implements OnInit {
     }
 
     valid(): boolean {
+        if (!this.locale) {
+            return false;
+        }
         if (!this.repeatIdent) {
             return false;
         }
