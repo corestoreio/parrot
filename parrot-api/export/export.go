d26c649786e287a1994eb1f766343d67c65efb73
Anthony Najjar <anthonynsimon@gmail.com>
2016-12-20 13:13:42 +0100

    Add doc to export package

--- parrot-api/export/export.go
+++ parrot-api/export/export.go
@@ -1,7 +1,9 @@
+// Package export handles the exporting of API data to common formats.
 package export
 
 import "github.com/anthonynsimon/parrot/parrot-api/model"
 
+// Exporter specifies the interface that must be specified for every format.
 type Exporter interface {
 	FileExtension() string
 	Export(*model.Locale) ([]byte, error)
