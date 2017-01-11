13a6a242987dbdc22ae7b0782ca629a2833d6251
Anthony Najjar <anthonynsimon@gmail.com>
2017-01-11 12:55:50 +0100

    Add parrot env to api docker build pipeline

--- parrot-api/scripts/server-start.sh
+++ parrot-api/scripts/server-start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Initializing API server..." && \
-    chmod +wx $PARROT_API_ROOT/scripts/install-gen-cert.sh && $PARROT_API_ROOT/scripts/install-gen-cert.sh && \
-    chmod +wx $PARROT_API_ROOT/scripts/gen-cert.sh && $PARROT_API_ROOT/scripts/gen-cert.sh && \
+    chmod +wx $PARROT_API_ROOT/scripts/install-gen-cert.sh && /bin/bash $PARROT_API_ROOT/scripts/install-gen-cert.sh && \
+    chmod +wx $PARROT_API_ROOT/scripts/gen-cert.sh && /bin/bash $PARROT_API_ROOT/scripts/gen-cert.sh && \
 
     echo "Building API server source files..." && \
     cd $PARROT_API_ROOT && go build && \
