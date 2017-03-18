29def47622225eab73605efc6f3e93ea15e2a816
Todes <gatopulos@hotmail.com>
2017-03-18 18:40:12 +0200

    Update karma configuration with the rc cli dependencies  (#49)
    * add dependencies for karma
    
    * update karma configuration with cli rc update
    
    * add first test to check the new karma config

---
+++ web-app/src/app/app.component.spec.ts
@@ -0,0 +1,41 @@
+
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { AppComponent } from './app.component';
+
+
+describe('AppComponent', () => {
+
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent,
+            ],
+            imports: [ RouterTestingModule ]
+        }).overrideComponent( AppComponent,{
+            set:{
+                template: `
+                    <span>{{ title }}</span>
+                    <section class="main">
+                        <router-outlet></router-outlet>
+                    </section>
+                `
+            }
+        } ).compileComponents();
+       
+        
+    }));
+
+    it('should create the app', async(() => {
+        let fixture = TestBed.createComponent(AppComponent);
+        let app = fixture.debugElement.componentInstance;
+        expect(app).toBeTruthy();
+    }));
+
+    it(`should have as title 'Parrot'`, async(() => {
+        let fixture = TestBed.createComponent(AppComponent);
+        let app = fixture.debugElement.componentInstance;
+        expect(app.title).toEqual('Parrot');
+    }));
+});
